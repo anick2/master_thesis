c31be2d0f4c61723d4231a647644ed3e7505566a
anick2704 <anick2704@yandex-team.ru>
2023-04-04 18:18:44 +0300

    add abae

---
+++ abae/config.py
@@ -0,0 +1,15 @@
+# model
+batch_size = 50
+ortho_reg = 0.2
+aspects_number = 25
+epochs = 1
+negative_samples = 5
+max_len = 201
+log_progress_steps = 1000
+
+optimizer = 'adam'
+data_path = 'restraunts_big.txt'
+word2vec_path = '../word_vectors/restraunts_big.w2v'
+
+
+
